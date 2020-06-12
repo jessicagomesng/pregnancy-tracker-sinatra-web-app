c134e53aa13b5981c7d6bbf9ff313f9334af2e34
jessicagomesng <jgnpeluso@gmail.com>
2020-06-12 13:02:35 +0100

    added methods to entry controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,8 +1,13 @@
 class EntriesController < ApplicationController 
     get '/entries' do 
-        @user = User.find_by_id(session[:user_id])
-        @entries = User.entries
+        if logged_in? 
+            @user = User.find_by_id(session[:user_id])
+            @entries = @user.entries
 
+            erb :'/entries/index'
+        else 
+            redirect '/login'
+        end 
     end 
 
     get '/entries/new' do 
@@ -17,7 +22,46 @@ class EntriesController < ApplicationController
     end 
 
     post '/entries' do 
-        binding.pry 
+        @user = User.find_by_id(session[:user_id])
 
+        if params[:entry][:date] == "" || params[:entry][:weeks] == ""
+            redirect '/entries/new'
+            #flash message - these fields cannot be left lbank
+        else 
+            @entry = Entry.new
+
+            params["entry"].each do |key, value|
+                if !value.nil?
+                    if key == "symptom_ids"
+                        value.each do |symptom_id|
+                            symptom = Symptom.find_by_id(symptom_id)
+                            @entry.symptoms << symptom
+                        end 
+                    else 
+                        @entry[key] = value 
+                    end 
+                end 
+            end 
+
+            @entry.save 
+            @user.entries << @entry 
+            
+            redirect "/entries/#{@entry.date_slug}"
+        end 
     end 
+
+    get '/entries/:slug' do 
+        if logged_in? 
+            @user = User.find_by_id(session[:user_id])
+            @entry = @user.entries.find_by_date_slug(params[:slug])
+
+            erb :'/entries/show'
+        else 
+            redirect '/login'
+        end 
+    end 
+
+    #edit 
+    #delete
+    #list of entries
 end 
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,4 +2,17 @@ class Entry < ActiveRecord::Base
     belongs_to :user
     has_many :entries_symptoms
     has_many :symptoms, through: :entries_symptoms
+
+    #need to slugify date
+
+    def date_slug
+        self.date.to_s
+    end 
+
+    def self.find_by_date_slug(date_slug)
+        self.all.detect do |entry|
+            entry.date_slug == date_slug
+        end 
+    end
+    
 end 
