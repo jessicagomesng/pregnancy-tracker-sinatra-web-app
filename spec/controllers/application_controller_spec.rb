2e1eb2c2fc77d518914de2683abfe7ee7185b781
jessicagomesng <jgnpeluso@gmail.com>
2020-06-18 11:45:15 +0100

    adding tests

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -50,6 +50,87 @@ describe ApplicationController do
             post '/signup', params
             expect(last_response.location).to include('/signup')
         end
+
+        it 'does not let a user sign up without a password' do 
+            params = {
+                :username => "hithere",
+                :email => "chocolate@aol.com",
+                :password => ""
+            }
+
+            post '/signup', params
+            expect(last_response.location).to include('/signup')
+        end 
+
+        it 'does not let a user sign up without an email' do 
+            params = {
+                :username => "hithere",
+                :email => "",
+                :password => "EasterEggs123"
+            }
+
+            post '/signup', params 
+            expect(last_response.location).to include('/signup')
+        end 
+
+        it 'does not let a logged in user view the sign up page' do 
+            params = {
+                :username => "hithere", 
+                :email => "",
+                :password => "EasterEggs123"
+            }
+
+            post '/signup', params
+            get '/signup'
+            expect(last_response.location).to include('/account')
+        end 
+    end 
+
+    describe "login" do 
+        it 'loads the login page' do 
+            get '/login'
+            expect(last_response.status).to eq(200)
+        end 
+
+        it 'loads the account homepage after successful login' do 
+            user = User.create(:username => "hotdoggy", :email => "sausagedogs@hotmail.com", :password => "ImaPup45")
+
+            params = {
+                :username => "hotdoggy",
+                :password => "ImaPup45"
+            }
+
+            post '/login', params
+            expect(last_response.status).to eq(302)
+            follow_redirect!
+            expect(last_response.status).to eq(200)
+            expect(last_response.location).to include('/account')
+        end 
+
+        it 'does not let a logged in user view the login page' do 
+            user = User.create(:username => "hotdoggy", :email => "sausagedogs@hotmail.com", :password => "ImaPup45")
+
+            params = {
+                :username => "hotdoggy",
+                :password => "ImaPup45"
+            }
+
+            post '/login', params 
+            get '/login'
+            expect(last_response.location).to include('/account')
+        end 
+
+        it 'authenticates username/password combination' do 
+            user = User.create(:username => "hotdoggy", :email => "sausagedogs@hotmail.com", :password => "ImaPup45")
+
+            params = {
+                :username => "hotdoggy",
+                :password => "fakepassword"
+            }
+
+            post '/login', params 
+            expect(last_response.location).to include('/login')
+        end 
     end 
 
 end 
--- spec/models/01_user_spec.rb
+++ spec/models/01_user_spec.rb
@@ -1,6 +1,10 @@
-# require "spec_helper"
+require "spec_helper"
 
-# describe "User" do 
-#     before do 
-#         @user = User.create(:username => "preggoeggo", :email => "hi@gmail.com", :password => "Mynameis2")
-        
+describe "User" do 
+    # before do 
+    #     @user = User.create(:username => "preggoeggo", :email => "hi@gmail.com", :password => "Mynameis2")
+    it 'responds to authenticate method from has_secure_password' do 
+        @user = User.create(:username => "testing", :password => "Testing123", :email => "test@gmail.com")
+        expect(@user.authenticate("Testing123")).to be_truthy 
+    end 
+end
