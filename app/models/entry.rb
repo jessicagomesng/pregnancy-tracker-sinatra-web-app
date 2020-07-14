296a2d1222b27e3302e09516177e6cb66fbb6216
jessicagomesng <jgnpeluso@gmail.com>
2020-07-14 11:54:23 +0100

    deleted unnecessary methods

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,15 +2,5 @@ class Entry < ActiveRecord::Base
     belongs_to :user
     has_many :entries_symptoms
     has_many :symptoms, through: :entries_symptoms
-
-    def date_slug
-        self.date.to_s
-    end 
-
-    def self.find_by_date_slug(date_slug)
-        self.all.detect do |entry|
-            entry.date_slug == date_slug
-        end 
-    end
     
 end 
